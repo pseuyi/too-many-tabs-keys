22596a28e1c339d31baab102da542332e571ff82
mithunder <mselvaratnam@gmail.com>
2017-01-06 14:29:12 -0500

    diminished chords

--- main.js
+++ main.js
@@ -8,19 +8,27 @@ var polySynth = new Tone.PolySynth(6, Tone.Synth, {
 
 console.log("MAIN JS")
 
-function playSound(steps){
-  let counter = steps, baseFrequency = 220;
-  while (counter > 0){
-    baseFrequency *= Math.pow(2,1/12)
-    counter--
-  }
-  polySynth.triggerAttackRelease(baseFrequency, 0.5)
+function playNote(steps){
+    let counter = steps, baseFrequency = 220;
+    while (counter > 0){
+      baseFrequency *= Math.pow(2,1/12)
+      counter--
+    }
+    if (steps < 12) {
+      polySynth.triggerAttackRelease(baseFrequency, 0.5)
+    } else { // plays chords if over tab 12
+      let third = baseFrequency * Math.pow(2,1/12) * Math.pow(2,1/12) * Math.pow(2,1/12);
+      let fifth = third * Math.pow(2,1/12) * Math.pow(2,1/12) * Math.pow(2,1/12);
+      console.log(baseFrequency, third, fifth)
+      polySynth.triggerAttackRelease([baseFrequency,third, fifth], 0.5)
+
+    }
 }
 
 chrome.tabs.onActivated.addListener(function (tab){
   chrome.tabs.query({
-      active: true              // Select active tabs
+      active: true
   }, function (activeTabs) {
-    playSound(activeTabs[0].index)
+    playNote(activeTabs[0].index)
   })
 })
