5082aa0eafc10cfcbcd98ef378669a00765b8d92
mithunder <mselvaratnam@gmail.com>
2017-01-06 16:16:30 -0500

    hackey mute functionality

--- main.js
+++ main.js
@@ -7,27 +7,26 @@ chrome.tabs.onCreated.addListener(() => {
 	chrome.tabs.query({}, (tabs) => {
 		openTabs = tabs.length;
 		checkTabs(openTabs)
-	})	
+	})
 })
 chrome.tabs.onRemoved.addListener(() => {
 	chrome.tabs.query({}, (tabs) => {
 		openTabs = tabs.length;
+    // if (openTabs < 10) chrome.tabs.onActivated.removeListener();
 		checkTabs(openTabs)
-	})	
+	})
 })
 
 var checkTabs = (length) =>{
-	if(length>10) {
+  console.log("LENGTH", length)
 		// transform tabs to keys
     chrome.tabs.onActivated.addListener(function (tab){
       chrome.tabs.query({
           active: true
       }, function (activeTabs) {
-        playNote(activeTabs[0].index)
+        playNote(activeTabs[0].index, length)
       })
     })
-
-	}
 }
 
 var polySynth = new Tone.PolySynth(6, Tone.Synth, {
@@ -37,18 +36,30 @@ var polySynth = new Tone.PolySynth(6, Tone.Synth, {
  "volume": -12
 }).toMaster();
 
-function playNote(steps){
+var dist = new Tone.Distortion(0.1).toMaster();
+polySynth.connect(dist)
+
+function playNote(steps, length){
+  // console.log("PLAYNOTE", steps, length)
+  if (length >= 10) Tone.Master.mute = false;
     let counter = steps, baseFrequency = 220;
     while (counter > 0){
       baseFrequency *= Math.pow(2,1/12)
       counter--
     }
-    if (steps < 12) {
+    if (steps < 10 && length < 10) {
+      console.log("FIRST CASE")
+      Tone.Master.mute = true;
+      // chrome.tabs.onActivated.removeListener()
+    }
+    else if (steps < 12 && length >= 10) {
       polySynth.triggerAttackRelease(baseFrequency, 0.5)
-    } else { // plays chords if over tab 12
+    } else if (steps >=12 && length >= 10){ // plays chords if over tab 12
       let third = baseFrequency * Math.pow(2,1/12) * Math.pow(2,1/12) * Math.pow(2,1/12);
       let fifth = third * Math.pow(2,1/12) * Math.pow(2,1/12) * Math.pow(2,1/12);
-      console.log(baseFrequency, third, fifth)
+      // console.log(baseFrequency, third, fifth)
+      dist.distortion+=0.05;
+      console.log(dist)
       polySynth.triggerAttackRelease([baseFrequency,third, fifth], 0.5)
 
     }
