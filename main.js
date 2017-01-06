c247c59c4f9d688882ef558c4ec4e8cd8869af36
freda nada <fredasnada@gmail.com>
2017-01-06 13:19:32 -0500

    add tab query

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 /* code here */
-var polySynth = new Tone.PolySynth(6, Tone.Synth, {
-  "oscillator" : {
-    "partials" : [0, 2, 3, 4],
-  },
-  "volume": -12
-}).toMaster();
 
-console.log("MAIN JS")
+var openTabs;
+chrome.tabs.query({}, function(tabs){
+	openTabs = tabs.length;
+	console.log('num open', openTabs)
+})
 
-polySynth.triggerAttack("C4");
+if(openTabs>10) {
+	var polySynth = new Tone.PolySynth(6, Tone.Synth, {
+	  "oscillator" : {
+	    "partials" : [0, 2, 3, 4],
+	  },
+	  "volume": -12
+	}).toMaster();
+
+	console.log("MAIN JS")
+
+	polySynth.triggerAttack("C4");
+	
+}
